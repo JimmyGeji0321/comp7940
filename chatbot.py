a9dd3282df8030e9632f0a0e27138913d54e9f22
Geji <22451102@life.hkbu.edu.hk>
2023-04-08 21:45:36 +0800

    add 2

--- chatbot.py
+++ chatbot.py
@@ -1,32 +1,118 @@
-import configparser
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-## chatbot.py
+# chatbot.py
 import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 # The messageHandler is used for all message updates
 import configparser
 import logging
+# import redis
+# import os
+
+#global redis1
+import pymysql
+
+
+def db_connect():
+    db = pymysql.connect(host='cloudcomputing.c8zsz8bm7sbi.us-east-1.rds.amazonaws.com', user='admin', password='swQ50wDD9xXEuGZwDpva', database='chatbot')
+    return db
+
+
+def show_reviews():
+    db = db_connect()
+    cursor = db.cursor()
+    sql = "SELECT * FROM review"
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        db.close()
+        return results
+    except:
+        print("Error: unable to fetch data")
+
+
+def add_review(movie_name, movie_review):
+    db = db_connect()
+    cursor = db.cursor()
+    sql = "INSERT INTO review(movie_name, movie_review) VALUES ('%s', '%s')" % (movie_name, movie_review)
+    try:
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+    except:
+        print("Error")
+
 
 def main():
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
+
     # You can set this logging module, so you will know when and why things do not work as expected
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    # register a dispatcher to handle message: here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    # # register a dispatcher to handle message: here we register an echo dispatcher
+    menu_handler = MessageHandler(Filters.text & (~Filters.command), show_menu)
+    dispatcher.add_handler(menu_handler)
+
+    # # on different commands - answer in Telegram
+    # dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("photo", photo))
+    dispatcher.add_handler(CommandHandler("video", video))
+    dispatcher.add_handler(CommandHandler("review", review))
+
     # To start the bot:
     updater.start_polling()
     updater.idle()
-def echo(update, context):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
-    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+
+
+def show_menu(update, context):
+    update.message.reply_text('''Hello! Welcome to CC chatbot! 
+Please select the following functions:
+
+1. Type '/photo' to see hiking photos 
+ex: /photo victoria, /photo changzhou
+2. Type '/video' to see cooking videos 
+ex: /video cooking
+3. Type '/review' to read TV show reviews
+ex: /review read''')
+
+
+def photo(update: Update, context: CallbackContext) -> None:
+    msg = context.args[0]
+    if msg == 'victoria':
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/t1-2.jpg', 'rb'))
+    elif msg == 'changzhou':
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('img/t2-2.jpg', 'rb'))
+    else:
+        update.message.reply_text("Sorry, I didn't have any photos related to " + msg + "!")
+
+
+def video(update: Update, context: CallbackContext) -> None:
+    msg = context.args[0]
+    if msg == 'cooking':
+        context.bot.send_video(chat_id=update.effective_chat.id, video=open('video/2.mp4', 'rb'))
+    else:
+        update.message.reply_text("Sorry, I didn't have any videos related to " + msg + "!")
+
+
+def review(update: Update, context: CallbackContext) -> None:
+    msg = context.args[0]
+    # if msg == 'write':
+    #     movie_name = context.args[1]
+    #     movie_review = context.args[2]
+    #     # add_review(movie_name, movie_review)
+    #     print(movie_name, movie_review)
+    if msg == 'read':
+        results = show_reviews()
+        for i in results:
+            update.message.reply_text(i[1] + ': ' + i[2])
+    else:
+        update.message.reply_text("Please input the right command!")
+
+
 if __name__ == '__main__':
     main()
